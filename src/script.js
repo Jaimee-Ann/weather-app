9e4e9eaeac7ce98ec8ff4266b69d2444711f126a
Jaimee-Ann <jaimeeann@gmail.com>
2021-01-26 18:39:16 +1100

    Remove precipitation

--- src/script.js
+++ src/script.js
@@ -6,7 +6,7 @@ function showWeatherCondition(response) {
     document.querySelector("#description").innerHTML =
       response.data.weather[0].main;
     document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-    document.querySelector("#precipitation").innerHTML = response.data.main.precipitation;
+    document.querySelector("#precipitation").innerHTML = response.data.main.precipitation.value;
     document.querySelector("#wind").innerHTML = Math.round(
       response.data.wind.speed * 3.6
     );
