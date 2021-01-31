da692eae3bc30a9293b4ae0883ed27dd93db6fde
Jaimee-Ann <jaimeeann@gmail.com>
2021-01-31 11:10:52 +1100

    Weather forecast js

--- src/script.js
+++ src/script.js
@@ -13,11 +13,71 @@ function showWeatherCondition(response) {
    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
    iconElement.setAttribute("alt", response.data.weather[0].icon);
 }
+
+
+  function formatDate(timestamp){
+    let date = new Date(timestamp);
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
+    let day = days[date.getDay()];
+    return`${day} ${formatHours(timestamp)}`;  
+  }
+
+function formatHours(timestamp){
+  let date = new Date(timestamp);
+  let hours = currentTime.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = currentTime.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+    
+  }
+  return `${hours}:${minutes}`;
+}
+
+
   
+function displayForecast(response){
+let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML = null;
+let forecast = null;
+
+for (let index = 0; index < 6; index++) {
+  let forecast = (response.data.list[index]);
+  forecastElement.innerHTML = `
+  <div class="row">
+    <div class="col-12 col-md-2 mb-3">
+      <div class="card">
+        <div class="card-body">
+          <p class="card-text">${formatHours(forecast.dt * 1000)}</p>
+          <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"
+          alt=" "
+          id="icon"
+          />
+          <h5 class="card-text">${Math.round(forecast.main.temp_min)}° <strong>${Math.round(forecast.main.temp_min)}°</strong></h5>
+        </div>
+      </div>
+    </div>
+  `;  
+}
+}
+
   function search(city) {
     let apiKey = "8aed1bf91e607b096a113c2dd8fde03b";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
     axios.get(apiUrl).then(showWeatherCondition);
+
+    apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`;
+    axios.get(apiURL).then(displayForecast);
   }
   
   function liveSearch(event) {
@@ -56,31 +116,29 @@ function showWeatherCondition(response) {
     fahrenheitLink.classList.remove("active");
     temperatureElement.innerHTML = Math.round(celsiusTemp);
   }
-  
+   // Date & Time
+   let dateElement = document.querySelector("#todayDate");
+   let currentTime = new Date();
+   let hours = currentTime.getHours();
+   if (hours < 10) {
+     hours = `0${hours}`;
+   }
+   let minutes = currentTime.getMinutes();
+   if (minutes < 10) {
+     minutes = `0${minutes}`;
+   }
+   let dayIndex = currentTime.getDay();
+   let days = [
+     "Sunday",
+     "Monday",
+     "Tuesday",
+     "Wednesday",
+     "Thursday",
+     "Friday",
+     "Saturday"
+   ];
+   dateElement.innerHTML = `${days[dayIndex]} ${hours}:${minutes}`;
 
-  // Date & Time
-  let dateElement = document.querySelector("#todayDate");
-  let currentTime = new Date();
-  let hours = currentTime.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = currentTime.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  let dayIndex = currentTime.getDay();
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-  dateElement.innerHTML = `${days[dayIndex]} ${hours}:${minutes}`;
-  
   // Search engine
   let searchForm = document.querySelector("#searchBar");
   searchForm.addEventListener("submit", liveSearch);
