5c6e30659fdb774a0d8626967622bcca50768290
Jaimee-Ann <jaimeeann@gmail.com>
2021-02-01 14:48:14 +1100

    Fixed CSS html and js for rows of forecast

--- src/script.js
+++ src/script.js
@@ -45,31 +45,31 @@ function showWeatherCondition(response) {
 
 
   
-function displayForecast(response){
-let forecastElement = document.querySelector("#forecast");
-forecastElement.innerHTML = null;
-let forecast = null;
-
-for (let index = 0; index < 6; index++) {
-  let forecast = (response.data.list[index]);
-  forecastElement.innerHTML += `
-  <div class="row">
-  <div class="col-12 col-md mb-3">
-      <div class="card">
-        <div class="card-body">
-          <p class="card-text">${formatHours(forecast.dt * 1000)}</p>
-          <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"
-          alt=" "
-          id="icon"
-          />
-          <h5 class="card-text">${Math.round(forecast.main.temp_min)}° <strong>${Math.round(forecast.main.temp_max)}°</strong></h5>
+  function displayForecast(response) {
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = null;
+    let forecast = null;
+    for (let index = 0; index < 6; index++) {
+      let forecast = response.data.list[index];
+      forecastElement.innerHTML += `
+    <div class="col-12 col-md mb-3">
+        <div class="card">
+          <div class="card-body">
+            <p class="card-text">${formatHours(forecast.dt * 1000)}</p>
+            <img src="http://openweathermap.org/img/wn/${
+              forecast.weather[0].icon
+            }@2x.png"
+            alt=" "
+            id="icon"
+            />
+            <h5 class="card-text">${Math.round(
+              forecast.main.temp_min
+            )}° <strong>${Math.round(forecast.main.temp_max)}°</strong></h5>
+          </div>
         </div>
-      </div>
-    </div>
-  `;  
-}
-}
-
+    `;
+    }
+  }
   function search(city) {
     let apiKey = "8aed1bf91e607b096a113c2dd8fde03b";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
